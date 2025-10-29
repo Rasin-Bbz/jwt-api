485325d6e98ad843e7898bd3d0a838035fce0b0d
Rasin Bb'z <developerrasin69@gmail.com>
2025-10-29 21:59:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def process_token(uid, password):
         'Expect': "100-continue",
         'X-GA': "v1 1",
         'X-Unity-Version': "2018.4.11f1",
-        'ReleaseVersion': "OB50"
+        'ReleaseVersion': "OB51"
     }
     edata = bytes.fromhex(hex_encrypted_data)
 
